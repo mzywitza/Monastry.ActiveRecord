4f3c60beb60344382f7aa24555e199b253193a7d
Markus Zywitza <markus.zywitza@gmail.com>
2011-06-03 20:38:17 +0200

    Added NhConversationContext

---
+++ source/Monastry.ActiveRecord.NHibernate/NhConversationContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monastry.ActiveRecord.NHibernate
+{
+    public class NhConversationContext : ConversationContext<INhConversation, INhScope>, INhConversationContext
+    {
+    }
+}
