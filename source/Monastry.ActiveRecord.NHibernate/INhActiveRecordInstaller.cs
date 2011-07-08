37f770f49f2f4f03a925e02ae41b31bd59759528
Markus Zywitza <markus.zywitza@gmail.com>
2011-07-08 20:59:43 +0200

    Installer interface for NHibernate added.

---
+++ source/Monastry.ActiveRecord.NHibernate/INhActiveRecordInstaller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate.Cfg;
+
+namespace Monastry.ActiveRecord
+{
+    /// <summary>
+    /// NHibernate specific <see cref="IActiveRecordInstaller"/>.
+    /// </summary>
+    public interface INhActiveRecordInstaller : IActiveRecordInstaller
+    {
+        /// <summary>
+        /// Obtains the NHibernate configuration. The object must be fully 
+        /// configured because it will be used to create the <see cref="ISessionFactory"/>
+        /// after the call.
+        /// </summary>
+        /// <returns>The NHibernate configuration to use.</returns>
+        Configuration GetNhConfiguration();
+    }
+}
