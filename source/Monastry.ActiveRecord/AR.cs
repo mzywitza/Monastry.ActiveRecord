623195b297911f6156598d9b8a2753fc5e3577a9
Markus Zywitza <markus.zywitza@gmail.com>
2011-07-08 20:35:39 +0200

    XML docs added

--- source/Monastry.ActiveRecord/AR.cs
+++ source/Monastry.ActiveRecord/AR.cs
@@ -6,10 +6,30 @@ using Castle.Windsor;
 
 namespace Monastry.ActiveRecord
 {
+    /// <summary>
+    /// Central class for AR.
+    /// </summary>
     public class AR
     {
+        /// <summary>
+        /// This container is used by AR to obtain all internal services.
+        /// </summary>
         protected internal static IWindsorContainer Container {get; private set;}
 
+        /// <summary>
+        /// Registers the container for AR internal usage. The container must be
+        /// configured to resolve at least the following services:
+        /// <list type="bullet">
+        /// <item><see cref="IDao<>"/></item>
+        /// <item><see cref="IConversation"/></item>
+        /// <item><see cref="IConversationContext"/></item>
+        /// </list>
+        /// </summary>
+        /// <param name="container">The container instance to use.</param>
+        /// <exception cref="ArgumentException">
+        /// <see cref="ArgumentException"/> will be thrown when not all
+        /// necessary services are registered.
+        /// </exception>
         public static void Install(IWindsorContainer container)
         {
             if (!IsConfigured(container))
@@ -20,6 +40,10 @@ namespace Monastry.ActiveRecord
             Container = container;
         }
 
+        /// <summary>
+        /// Uses the container provided by the installer to configure AR.
+        /// </summary>
+        /// <param name="installer">The installer to use.</param>
         public static void Install(IActiveRecordInstaller installer)
         {
             AR.Install(installer.GetConfiguredContainer());
