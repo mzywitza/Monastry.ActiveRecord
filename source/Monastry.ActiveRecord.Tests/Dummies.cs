2a86fe65fbdff51f1aa4e7f740c430b9fb841e53
mzywitza <markus.zywitza@gmail.com>
2011-09-11 14:41:40 +0200

    Refactored tests

---
+++ source/Monastry.ActiveRecord.Tests/Dummies.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.Windsor;
+using Castle.MicroKernel.Registration;
+
+namespace Monastry.ActiveRecord.Tests
+{
+	public static class DummyFactory
+	{
+		public static IWindsorContainer CreateContainer()
+		{
+			var container = new WindsorContainer();
+			container.Register(
+				Component.For(typeof(IDao<>)).ImplementedBy(typeof(DummyDao<>)),
+				Component.For<IConversation>().ImplementedBy<DummyConversation>(),
+				Component.For<IConversationContext>().ImplementedBy<DummyConversationContext>());
+			return container;
+		}
+	}
+
+	#region Dummies
+	public class DummyDao<T> : IDao<T> where T : class
+	{
+
+		public IConversation CurrentConversation
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public void Save(T entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Add(T entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Replace(T entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public T Find(object id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public T Peek(object id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IQueryable<T> Linq()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete(T entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Forget(T entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class DummyConversation : IConversation
+	{
+		public void Cancel()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Commit()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Restart()
+		{
+			throw new NotImplementedException();
+		}
+
+		public ConversationCommitMode CommitMode
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool IsCanceled
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public void Execute(Action action)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void ExecuteSilently(Action action)
+		{
+			throw new NotImplementedException();
+		}
+
+		public event EventHandler<ConversationCanceledEventArgs> Canceled;
+
+		public IDisposable Scope()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IConversationContext Context
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class DummyConversationContext : IConversationContext
+	{
+
+		public IConversation CurrentConversation
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public void SetDefaultConversation(IConversation conversation)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void EndDefaultConversation()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IScope CurrentScope
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public void RegisterScope(IScope scope)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void ReleaseScope(IScope scope)
+		{
+			throw new NotImplementedException();
+		}
+	}
+	#endregion  
+
+}
--- source/Monastry.ActiveRecord.Tests/InstallationTests.cs
+++ source/Monastry.ActiveRecord.Tests/InstallationTests.cs
@@ -15,12 +15,7 @@ namespace Monastry.ActiveRecord.Tests
 		[Test]
 		public void InstallationSucceedsForCorrectContainer()
 		{
-			var container = new WindsorContainer();
-			container.Register(
-				Component.For(typeof(IDao<>)).ImplementedBy(typeof(DummyDao<>)),
-				Component.For<IConversation>().ImplementedBy<DummyConversation>(),
-				Component.For<IConversationContext>().ImplementedBy<DummyConversationContext>());
-			AR.Install(container);
+			AR.Install(DummyFactory.CreateContainer());
 		}
 
 		[Test]
@@ -59,11 +54,7 @@ namespace Monastry.ActiveRecord.Tests
 		[Test]
 		public void InstallationSetsUsageOnInstaller()
 		{
-			var container = new WindsorContainer();
-			container.Register(
-				Component.For(typeof(IDao<>)).ImplementedBy(typeof(DummyDao<>)),
-				Component.For<IConversation>().ImplementedBy<DummyConversation>(),
-				Component.For<IConversationContext>().ImplementedBy<DummyConversationContext>());
+			var container = DummyFactory.CreateContainer();
 			var installer = MockRepository.GenerateStrictMock<IActiveRecordInstaller>();
 			installer.Expect(i => i.Usage).SetPropertyWithArgument(Usage.Web);
 			installer.Expect(i => i.GetConfiguredContainer()).Return(container);
@@ -72,150 +63,4 @@ namespace Monastry.ActiveRecord.Tests
 		}
 	}
 
-	#region Dummies
-	public class DummyDao<T> : IDao<T> where T: class
-	{
-
-		public IConversation CurrentConversation
-		{
-			get { throw new NotImplementedException(); }
-		}
-
-		public void Save(T entity)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void Add(T entity)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void Replace(T entity)
-		{
-			throw new NotImplementedException();
-		}
-
-		public T Find(object id)
-		{
-			throw new NotImplementedException();
-		}
-
-		public T Peek(object id)
-		{
-			throw new NotImplementedException();
-		}
-
-		public IQueryable<T> Linq()
-		{
-			throw new NotImplementedException();
-		}
-
-		public void Delete(T entity)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void Forget(T entity)
-		{
-			throw new NotImplementedException();
-		}
-	}
-
-	public class DummyConversation : IConversation
-	{
-		public void Cancel()
-		{
-			throw new NotImplementedException();
-		}
-
-		public void Commit()
-		{
-			throw new NotImplementedException();
-		}
-
-		public void Restart()
-		{
-			throw new NotImplementedException();
-		}
-
-		public ConversationCommitMode CommitMode
-		{
-			get
-			{
-				throw new NotImplementedException();
-			}
-			set
-			{
-				throw new NotImplementedException();
-			}
-		}
-
-		public bool IsCanceled
-		{
-			get { throw new NotImplementedException(); }
-		}
-
-		public void Execute(Action action)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void ExecuteSilently(Action action)
-		{
-			throw new NotImplementedException();
-		}
-
-		public event EventHandler<ConversationCanceledEventArgs> Canceled;
-
-		public IDisposable Scope()
-		{
-			throw new NotImplementedException();
-		}
-
-		public IConversationContext Context
-		{
-			get { throw new NotImplementedException(); }
-		}
-
-		public void Dispose()
-		{
-			throw new NotImplementedException();
-		}
-	}
-
-	public class DummyConversationContext : IConversationContext
-	{
-
-		public IConversation CurrentConversation
-		{
-			get { throw new NotImplementedException(); }
-		}
-
-		public void SetDefaultConversation(IConversation conversation)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void EndDefaultConversation()
-		{
-			throw new NotImplementedException();
-		}
-
-		public IScope CurrentScope
-		{
-			get { throw new NotImplementedException(); }
-		}
-
-		public void RegisterScope(IScope scope)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void ReleaseScope(IScope scope)
-		{
-			throw new NotImplementedException();
-		}
-	}
-	#endregion  
 }
